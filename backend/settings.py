5c206a8001c11a8e86d3c118a6931c8bf733a362
mrbin509 <bineetkumarsahu771@gmail.com>
2026-03-23 02:11:58 +0530

    Fixed referral system (optional), improved OTP flow, fraud safety, and models stability

--- backend/settings.py
+++ backend/settings.py
@@ -154,8 +154,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv('EMAIL_USER', 'your_email@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS', 'your_app_password')
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
 # 💳 RAZORPAY
