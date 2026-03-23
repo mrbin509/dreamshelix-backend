52e173ebec0f08cc79b8b4046ea666cfcd7d8895
mrbin509 <bineetkumarsahu771@gmail.com>
2026-03-23 14:17:48 +0530

    update settings

--- users/serializers.py
+++ users/serializers.py
@@ -12,7 +12,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
-
+from .models import CustomUser, Transaction, Wallet, Referral
 from rest_framework import serializers
 
 from .fraud_utils import (
